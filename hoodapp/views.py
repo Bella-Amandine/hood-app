0832d6a53682fefbf13c241337d866e69e0153f2
Bella Amandine <bellamandy45@gmail.com>
2021-02-11 19:36:51 +0200

    Add signup view function

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignupForm
 
 # Create your views here.
 def signup(request):
-    pass
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            
+            return redirect('login')
+    else:
+        form = SignupForm()
+        return render(request, 'registration/signup.html', {'form': form})
